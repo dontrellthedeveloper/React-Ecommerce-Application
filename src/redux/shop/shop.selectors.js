1bd0ebb6f04dee946597ac7944548ca22a7c7c11
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-12 07:00:49 -0700

    changed shop_data array to object for better searching speed

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import {createSelector} from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    women: 4,
-    men: 5
-}
 
 const selectShop = state => state.shop;
 
@@ -19,7 +12,5 @@ export const selectCollection = collectionUrlParam =>
 createSelector(
     [selectCollections],
     collections =>
-    collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
-)
+    collections[collectionUrlParam]
+);
