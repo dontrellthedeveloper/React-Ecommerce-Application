42091488556ceacb96d7a9242d0c88008e98886b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-21 21:08:03 -0700

    added WithSpinner Higher Order Component

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -5,10 +5,21 @@ import {connect} from 'react-redux';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 import {updateCollections} from '../../redux/shop/shop.actions.js';
 
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+
 class ShopPage extends React.Component {
+   state = {
+      loading: true
+   };
+   
    unsubscribeFRomSnapshot = null;
 
    componentDidMount() {
@@ -21,17 +32,19 @@ class ShopPage extends React.Component {
          this.unsubscribeFRomSnapshot = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({loading: false})
          })
       })
    }
 
       render() {
-            const {match} = this.props;
+         const {match} = this.props;
+         const {loading} = this.state;
             return (
 
                <div className='shop-page'>
-                  <Route exact path={`${match.path}`} component={CollectionsOverview} />
-                  <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}/>
+                  <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} />
+                  <Route exact path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>}/>
                </div>
       ); 
    }
