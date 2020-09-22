1f308f616ee5809f9ad2bf48ab691dc1912b465d
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-09-22 13:06:13 -0700

    added promise pattern to interact with firebase

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -26,14 +26,10 @@ class ShopPage extends React.Component {
       const {updateCollections} = this.props;
       const collectionRef = firestore.collection('collections');
 
-      collectionRef.onSnapshot(async snapshot => {
-         convertCollectionsSnapshotToMap(snapshot);
-
-         this.unsubscribeFRomSnapshot = collectionRef.onSnapshot(async snapshot => {
+      collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false})
-         })
       })
    }
 
